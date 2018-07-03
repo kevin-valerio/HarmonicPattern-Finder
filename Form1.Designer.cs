9dffc7ffebf0090c5dacc2e160cb6cafec7481f7
Kevin Valerio <kevin.valerio@etu.univ-amu.fr>
2018-07-03 11:09:37 +0200

    Sources

---
+++ Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace HarmonicPatternFinder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.XD = new System.Windows.Forms.TextBox();
+            this.BD = new System.Windows.Forms.TextBox();
+            this.XB = new System.Windows.Forms.TextBox();
+            this.AC = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.AB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.XD);
+            this.groupBox1.Controls.Add(this.BD);
+            this.groupBox1.Controls.Add(this.XB);
+            this.groupBox1.Controls.Add(this.AC);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.AB);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(337, 255);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Informations";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(279, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Find";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // XD
+            // 
+            this.XD.Location = new System.Drawing.Point(101, 186);
+            this.XD.Name = "XD";
+            this.XD.Size = new System.Drawing.Size(201, 20);
+            this.XD.TabIndex = 9;
+            // 
+            // BD
+            // 
+            this.BD.Location = new System.Drawing.Point(101, 151);
+            this.BD.Name = "BD";
+            this.BD.Size = new System.Drawing.Size(200, 20);
+            this.BD.TabIndex = 8;
+            // 
+            // XB
+            // 
+            this.XB.Location = new System.Drawing.Point(101, 115);
+            this.XB.Name = "XB";
+            this.XB.Size = new System.Drawing.Size(201, 20);
+            this.XB.TabIndex = 7;
+            // 
+            // AC
+            // 
+            this.AC.Location = new System.Drawing.Point(101, 73);
+            this.AC.Name = "AC";
+            this.AC.Size = new System.Drawing.Size(201, 20);
+            this.AC.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 192);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Ratio XD";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Ratio BD";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Ratio XB";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ratio AC";
+            // 
+            // AB
+            // 
+            this.AB.Enabled = false;
+            this.AB.Location = new System.Drawing.Point(122, 34);
+            this.AB.Name = "AB";
+            this.AB.Size = new System.Drawing.Size(180, 20);
+            this.AB.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ratio AB\r\n(si bearish, AB = 0)";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::HarmonicPatternFinder.Properties.Resources.Screenshot_1;
+            this.pictureBox1.Location = new System.Drawing.Point(355, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(862, 668);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(95, 438);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 36);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Swith picture";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(9, 650);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(163, 13);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Copyright Kevin VALERIO - 2018";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1214, 672);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Opacity = 0.96D;
+            this.ShowIcon = false;
+            this.Text = "Harmonic Pattern Finder";
+            this.TopMost = true;
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox XD;
+        private System.Windows.Forms.TextBox BD;
+        private System.Windows.Forms.TextBox XB;
+        private System.Windows.Forms.TextBox AC;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox AB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HarmonicPatternFinder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(isInRange(AC, 0.382, 0.886)) {
+                if (isInRange(XB,0.618)) {
+                    if (isInRange(BD, 1.272, 1.618)) {
+                        if (isInRange(XD, 0.786)) {
+                            MessageBox.Show("Il s'agit d'un Bullish ou Bearish G-786");
+                        }
+                    }
+                }
+            }
+
+            if (isInRange(AC, 0.382, 0.886)) {
+                if (isInRange(XB, 0.382, 0.50)) {
+                    if (isInRange(BD, 1.618, 2.618)) {
+                        if (isInRange(XD, 0.886)) {
+                            MessageBox.Show("Il s'agit d'un Bullish ou Bearish B-886");
+                        }
+                    }
+                }
+            }
+
+            if (isInRange(AC, 0.382, 0.886)) {
+                if (isInRange(XB, 0.382)) {
+                    if (isInRange(BD, 2.0, 3.618)) {
+                        if (isInRange(XD, 1.13)) {
+                            MessageBox.Show("Il s'agit d'un Bullish ou Bearish AB-113");
+                        }
+                    }
+                }
+            }
+
+            if (isInRange(AC, 0.382, 0.886)) {
+                if (isInRange(XB, 0.786)) {
+                    if (isInRange(BD, 1.618, 2.24)) {
+                        if (isInRange(XD,1.272)) {
+                            MessageBox.Show("Il s'agit d'un Bullish ou Bearish B-1272");
+                        }
+                    }
+                }
+            }
+
+            if (isInRange(AC, 0.382, 0.886)) {
+                if (isInRange(XB, 0.382, 0.50)) {
+                    if (isInRange(BD, 1.618, 2.618)) {
+                        if (isInRange(XD, 0.886, 0.886)) {
+                            MessageBox.Show("Il s'agit d'un Bullish ou Bearish B-886");
+                        }
+                    }
+                }
+            }
+
+            if (isInRange(AC, 0.382, 0.886)) {
+                if (isInRange(XB, 0.382, 0.618)) {
+                    if (isInRange(BD, 2.24, 3.618)) {
+                        if (isInRange(XD, 1.618)) {
+                            MessageBox.Show("Il s'agit d'un Bullish ou Bearish C-1618");
+                        }
+                    }
+                }
+            }
+
+            if (isInRange(AC, 0.382, 0.886)) {
+                if (isInRange(XB, 0.886)) {
+                    if (isInRange(BD, 2.0, 3.618)) {
+                        if (isInRange(XD, 1.618)) {
+                            MessageBox.Show("Il s'agit d'un Bullish ou Bearish DC-1618");
+                        }
+                    }
+                }
+            }
+
+            if (isInRange(AC, 1.13, 1.618)) {                 
+                    if (isInRange(BD, 1.618, 2.24)) {
+                        if (isInRange(XD, 0.886, 1.13)) {
+                            MessageBox.Show("Il s'agit d'un Bullish ou Bearish  S-886");
+                    }                    
+                }
+            }
+
+            if (isInRange(AC, 1.13, 1.618)) {
+                    if (isInRange(BD, 1.618, 2.24)) {
+                    MessageBox.Show("Il s'agit d'un 50% reversal (à vérifier pour OC pas calculé)");
+                        
+                    }
+                
+            } 
+
+        }
+
+        private Boolean isInRange(TextBox what, Double val1, Double val2 = 3121.24234)
+        {
+            if(val2 == 3121.24234) { val2 = val1; }
+            if (what.Text.Contains(".")) what.Text = what.Text.Replace(".", ",");
+            if (Convert.ToDouble(what.Text) <= val2 && Convert.ToDouble(what.Text) >= val1)
+                return true;
+            else
+                return false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(button2.ForeColor == Color.Red) {
+                button2.ForeColor = Color.Green;
+            } else {         
+                button2.ForeColor = Color.Red;
+            }
+
+            if (pictureBox1.Image.Size.Width == 1045  && pictureBox1.Image.Size.Height == 773 ) {
+                pictureBox1.Image = Properties.Resources.Screenshot_2;
+            } else {
+                pictureBox1.Image = Properties.Resources.Screenshot_1;
+            }
+        }
+    }
+}
